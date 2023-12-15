ce1d48a61a799544fc120b4f7d9e7f85b7418893
ian <i.cicarelli03@ufromail.cl>
2023-12-14 22:28:05 -0300

    commit menu

--- Main.py
+++ Main.py
@@ -4,9 +4,7 @@ from Utils import Utils
 class Main:
     @staticmethod
     def iniciar():
-        Utils.mostrar_artistas()
-        numero_artista = int(input("Seleccione el número del artista a visualizar: "))
-        Utils.mostrar_datos_artista(numero_artista)
+        Utils.menu()
 
 if __name__ == "__main__":
     Main.iniciar()
--- Utils.py
+++ Utils.py
@@ -34,7 +34,7 @@ class Utils:
     @staticmethod
     def mostrar_datos_artista(numero_artista):
         datos = Utils.cargar_datos()
-        artistas = Utils.mostrar_artistas()
+        artistas = Utils.obtener_artistas()
         if datos and artistas:
             if 1 <= numero_artista <= len(artistas):
                 artista_seleccionado = artistas[numero_artista - 1]
@@ -73,3 +73,89 @@ class Utils:
                 print("No se encontraron álbumes.")
         else:
             print("No se pudieron cargar los datos.")
+
+    @staticmethod
+    def mostrar_canciones_artista(numero_artista):
+        datos = Utils.cargar_datos()
+        artistas = Utils.obtener_artistas()
+        if datos and artistas:
+            if 1 <= numero_artista <= len(artistas):
+                artista_seleccionado = artistas[numero_artista - 1]
+                for sello in datos['sellos_discograficos']:
+                    for artista in sello['artistas']:
+                        if artista['nombre'] == artista_seleccionado:
+                            print(f"Canciones del artista '{artista['nombre']}':")
+                            for cancion in artista['canciones']:
+                                print(f"- {cancion['nombre']} ({cancion['fecha']}) - Duración: {cancion['duracion']}")
+                            return
+                print("El artista seleccionado no fue encontrado.")
+            else:
+                print("El número del artista seleccionado es inválido.")
+        else:
+            print("No se pudieron cargar los datos o no hay artistas para seleccionar.")    
+
+    @staticmethod
+    def mostrar_albums_artista(numero_artista):
+        datos = Utils.cargar_datos()
+        artistas = Utils.obtener_artistas()
+        if datos and artistas:
+            if 1 <= numero_artista <= len(artistas):
+                artista_seleccionado = artistas[numero_artista - 1]
+                for sello in datos['sellos_discograficos']:
+                    for artista in sello['artistas']:
+                        if artista['nombre'] == artista_seleccionado:
+                            if 'albumes' in artista and artista['albumes']:
+                                print(f"Álbumes del artista '{artista['nombre']}':")
+                                for album in artista['albumes']:
+                                    print(f"- {album['nombre']} ({album['fecha_publicacion']})")
+                            else:
+                                print(f"No se encontraron álbumes para el artista '{artista['nombre']}'.")
+                            return
+                print("El artista seleccionado no fue encontrado.")
+            else:
+                print("El número del artista seleccionado es inválido.")
+        else:
+            print("No se pudieron cargar los datos o no hay artistas para seleccionar.")   
+
+    @staticmethod
+    def obtener_artistas():
+        datos = Utils.cargar_datos()
+        if datos:
+            artistas = []
+            for sello in datos['sellos_discograficos']:
+                for artista in sello['artistas']:
+                    artistas.append(artista['nombre'])
+            if artistas:
+                return artistas
+            else:
+                print("No se encontraron artistas.")
+                return None
+        else:
+            print("No se pudieron cargar los datos.")
+            return None
+        
+
+    @staticmethod
+    def menu():
+        while True:
+            print("HARMONY-LIB")
+            print("Menú:")
+            print("1. Ver las canciones de un artista")
+            print("2. Ver los álbumes de un artista")
+            print("0. Salir")
+            
+            opcion = input("Ingrese el número de la opción que desea: ")
+            
+            if opcion == '1':
+                Utils.mostrar_artistas()
+                numero_artista = int(input("Seleccione el número del artista para ver sus canciones: "))
+                Utils.mostrar_canciones_artista(numero_artista)
+            elif opcion == '2':
+                Utils.mostrar_artistas()
+                num_artista = int(input("Seleccione el número para ver los álbumes del artista: "))
+                Utils.mostrar_albums_artista(num_artista)
+            elif opcion == '0':
+                print("Saliendo del menú.")
+                break
+            else:
+                print("Opción no válida. Ingrese un número válido de opción.")
